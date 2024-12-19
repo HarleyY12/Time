0fc91b2abccb6839b1d21682dd7e1ce6d1bf2015
HarleyY12 <hyang2496@gmail.com>
2024-12-19 17:41:42 -0500

    Update Main.java
    I wrote the runner class

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,42 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        Time time1 = new Time(8,59,57);
-        System.out.println(time1);
+        Time time1 = new Time(18,5,59);
+        System.out.println("Expected result : 18:05:59 ");
+        System.out.println("Actual result : " + time1);
         time1.tick();
-        System.out.println(time1);
+        System.out.println("Expected result : 18:06:00 ");
+        System.out.println("Actual result : " + time1);
         time1.tick();
-        System.out.println(time1);
         time1.tick();
-        System.out.println(time1);
         time1.tick();
-        System.out.println(time1);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Expected result : 18:06:03 ");
+        System.out.println("Actual result : " + time1);
+        Time time2 = new Time(6,50,35);
+        System.out.println("Expected result : 06:50:35 ");
+        System.out.println("Actual result : " + time2);
+        Time time3 = new Time(23,59,59);
+        time3.tick();
+        System.out.println("Expected result : 00:00:00 ");
+        System.out.println("Actual result : " + time3);
+        time3.tick();
+        System.out.println("Expected result : 00:00:01 ");
+        System.out.println("Actual result : " + time3);
+        Time time4 = new Time(14,56,6);
+        System.out.println("Expected result : 14:56:06 ");
+        System.out.println("Actual result : " + time4);
+        time4.tick();
+        time4.tick();
+        time4.tick();
+        time4.tick();
+        time4.tick();
+        System.out.println("Expected result : 14:56:11 ");
+        System.out.println("Actual result : " + time4);
+        time4.add(time1);
+        System.out.println("Expected result : 09:02:14 ");
+        System.out.println("Actual result : " + time4);
+        System.out.println("Expected result : 18:06:03 ");
+        System.out.println("Actual result : " + time1);
     }
 }
+
